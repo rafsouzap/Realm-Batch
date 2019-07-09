8231511eb41403bb1af3efa291ddb3ffa8469e89
Rafael de Paula <rafsouzap@users.noreply.github.com>
2019-07-09 18:48:40 -0300

    Update AlamofireRequest.swift

--- RealmBatchSync/Service/AlamofireRequest.swift
+++ RealmBatchSync/Service/AlamofireRequest.swift
@@ -45,30 +45,3 @@ final class AlamofireRequest {
 	}
 	
 }
-
-
-
-//static func get <T: Object> (type: T.Type, success:() -> Void, fail:@escaping (_ error:NSError)->Void)->Void where T:Mappable, T:Meta {
-//
-//	Alamofire.request(type.url()).responseArray { (response: DataResponse<[T]>) in
-//		switch response.result {
-//		case .success(let items):
-//			autoreleasepool {
-//				do {
-//					let realm = try Realm()
-//					try realm.write {
-//						for item in items {
-//							realm.add(item, update: true)
-//						}
-//					}
-//				} catch let error as NSError {
-//					fail(error)
-//				}
-//			}
-//		case .failure(let error):
-//			fail(error as NSError)
-//		}
-//	}
-//
-//}
-
